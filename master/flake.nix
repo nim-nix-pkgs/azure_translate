a7cbf5bf45ad267fdc6c27335834724bd929586d
hugosenari <hugosenari@gmail.com>
2022-03-12 00:41:14 -0300

    chore: re index azure_translate

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Nim Library for Azure Cognitive Services Translate'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-azure_translate-master.flake = false;
+  inputs.src-azure_translate-master.owner = "williamhatcher";
+  inputs.src-azure_translate-master.ref   = "refs/heads/master";
+  inputs.src-azure_translate-master.repo  = "azure_translate";
+  inputs.src-azure_translate-master.type  = "github";
+  
+  inputs."nuuid".dir   = "nimpkgs/n/nuuid";
+  inputs."nuuid".owner = "riinr";
+  inputs."nuuid".ref   = "flake-pinning";
+  inputs."nuuid".repo  = "flake-nimble";
+  inputs."nuuid".type  = "github";
+  inputs."nuuid".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nuuid".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-azure_translate-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-azure_translate-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_3_0/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Nim Library for Azure Cognitive Services Translate'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-azure_translate-v0_3_0.flake = false;
+  inputs.src-azure_translate-v0_3_0.owner = "williamhatcher";
+  inputs.src-azure_translate-v0_3_0.ref   = "refs/tags/v0.3.0";
+  inputs.src-azure_translate-v0_3_0.repo  = "azure_translate";
+  inputs.src-azure_translate-v0_3_0.type  = "github";
+  
+  inputs."nuuid".dir   = "nimpkgs/n/nuuid";
+  inputs."nuuid".owner = "riinr";
+  inputs."nuuid".ref   = "flake-pinning";
+  inputs."nuuid".repo  = "flake-nimble";
+  inputs."nuuid".type  = "github";
+  inputs."nuuid".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nuuid".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-azure_translate-v0_3_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-azure_translate-v0_3_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
