f89e1a9338d7d62d7062efaa36f62997dbd35d2e
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:54 -0300

    chore: re index azure_translate

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."azure_translate-master".dir   = "master";
+  inputs."azure_translate-master".owner = "nim-nix-pkgs";
+  inputs."azure_translate-master".ref   = "master";
+  inputs."azure_translate-master".repo  = "azure_translate";
+  inputs."azure_translate-master".type  = "github";
+  inputs."azure_translate-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."azure_translate-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."azure_translate-v0_3_0".dir   = "v0_3_0";
+  inputs."azure_translate-v0_3_0".owner = "nim-nix-pkgs";
+  inputs."azure_translate-v0_3_0".ref   = "master";
+  inputs."azure_translate-v0_3_0".repo  = "azure_translate";
+  inputs."azure_translate-v0_3_0".type  = "github";
+  inputs."azure_translate-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."azure_translate-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
